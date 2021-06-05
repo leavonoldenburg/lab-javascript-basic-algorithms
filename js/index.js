846e2a18726accf68644fc0f1a71ce0c4f65b457
Lea von Oldenburg <lea92.von.oldenburg@gmail.com>
2021-06-05 16:01:02 +0200

    solves iteration 1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = 'Hans';
+console.log(`"The driver's name is ${hacker1}."`);
 
+let hacker2 = 'Jürgen';
+console.log(`"The navigator's name is ${hacker2}."`);
 
 // Iteration 2: Conditionals
 

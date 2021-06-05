30466e4c3fcc5ca7fe9afeb97fd483f3b1fe0686
Lea von Oldenburg <lea92.von.oldenburg@gmail.com>
2021-06-05 17:18:28 +0200

    update iteration2 and 3

--- js/index.js
+++ js/index.js
@@ -15,18 +15,21 @@ if (hacker1.length > hacker2.length){
   }
 
 // Iteration 3: Loops
+let str = '';
 for (let i = 0; i < hacker1.length; i++){
-  // console.log(hacker1[i].toUpperCase());
-  process.stdout.write(hacker1[i].toUpperCase() + ' ');
+  str += ' ' + hacker1[i].toUpperCase();
 } 
+console.log(str);
 
-for (let i = hacker2.length; i >= 0; i--){
-  console.log(hacker2[i]);
+let str2 = ''
+for (let i = hacker2.length - 1; i >= 0; i--){
+  str2 += hacker2[i];
 }
+console.log(str2);
 
 if (hacker1.localeCompare(hacker2) < 0) {
   console.log(`"The driver's name goes first."`);
-} else if (hacker1.localeCompare(hacker2 > 0)){
+} else if (hacker1.localeCompare(hacker2) > 0){
   console.log(`"Yo, the navigator goes first definitely."`);
 } else {
   console.log(`"What?! You both have the same name?"`);
